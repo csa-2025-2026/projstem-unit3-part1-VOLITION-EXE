0e9584751e1a4e037ef3c8f6fd3a261c07f0d7a5
Vu Nguyen <vunguyen01202009@gmail.com>
2025-10-08 02:50:34 +0000

    g

--- Main.java
+++ Main.java
@@ -4,6 +4,17 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Rectangle r1 = new Rectangle(2, 5);
+    Rectangle r2 = new Rectangle(4, 2);
+
+    boolean isRotated = (r1.getLength() == r2.getWidth()) && (r1.getWidth() == r2.getLength());
+
+    boolean isCongruent = isRotated || r1.equals(r2);
+    boolean isSimilar = isCongruent || ( (r1.getLength() / r2.getLength()) == (r1.getWidth() / r2.getWidth()));
+
+
+    System.out.println(isRotated);
+    System.out.println(isCongruent);
+    System.out.println(isSimilar);
   }
 }
